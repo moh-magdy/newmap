a9cb49efefc065286b5a082953b2daa405e7cec9
moh-magdy <moh.mgd@gmail.com>
2017-10-02 23:25:30 +0200

    mod navbar

--- includes/templates/navbar.php
+++ includes/templates/navbar.php
@@ -1,39 +1,40 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container"><!-- /.container -->
         <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header navbar-right">
+        <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand " href="#">MAPLINK</a>
+          <a class="navbar-brand navbar-left" href="#">MAPLINK</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+
           <ul class="nav navbar-nav">
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">مزود الخدمة</a></li>
-                <li><a href="#">أتصل بنا</a></li>
-                <!--
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#">Separated link</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#">One more separated link</a></li>
--->
-              </ul>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">مزود الخدمة</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li><a href="#">أتصل بنا</a></li>                         
+                </ul>
             </li>
-            <li class="active"><a href="#">برمجه ويب <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">تصميم ويب</a></li>
-            <li><a href="#">تصميم الرسومات</a></li>
-            <li><a href="#">برمجه تطبيقات الدسك توب</a></li>
-            <li><a href="#">برمجه تطبيقات الموبايل</a></li>
             <li><a href="#">المزيد</a></li>
+            <li><a href="#">برمجه تطبيقات الموبايل</a></li>
+            <li><a href="#">برمجه تطبيقات الدسك توب</a></li>
+            <li><a href="#">تصميم الرسومات</a></li>
+            <li><a href="#">تصميم ويب</a></li>
+            <li class="active"><a href="#">برمجه ويب <span class="sr-only">(current)</span></a></li>
+            
+            
+            
+            
+            
+            
           </ul>
 
         </div><!-- /.navbar-collapse -->
