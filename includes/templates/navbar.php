9c8300e62d9cbf7760c5a71ebba3a9c57045f992
moh-magdy <moh.mgd@gmail.com>
2017-10-03 00:35:17 +0200

    fin nav

--- includes/templates/navbar.php
+++ includes/templates/navbar.php
@@ -2,29 +2,28 @@
       <div class="container"><!-- /.container -->
 
         <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
+        <div class="navbar-header navbar-right">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand navbar-left" href="#">MAPLINK</a>
+          <a class="navbar-brand " href="#">مابلينك</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-          <ul class="nav navbar-nav">
+          <ul class="nav navbar-nav navbar-left">
             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">..المزيد<span class="caret"></span></a>
                 <ul class="dropdown-menu">
                   <li><a href="#">مزود الخدمة</a></li>
                   <li role="separator" class="divider"></li>
                   <li><a href="#">أتصل بنا</a></li>                         
                 </ul>
             </li>
-            <li><a href="#">المزيد</a></li>
             <li><a href="#">برمجه تطبيقات الموبايل</a></li>
             <li><a href="#">برمجه تطبيقات الدسك توب</a></li>
             <li><a href="#">تصميم الرسومات</a></li>
@@ -33,6 +32,6 @@
           </ul>
 
         </div><!-- /.navbar-collapse -->
-        
+
       </div><!-- /.container -->
     </nav>
