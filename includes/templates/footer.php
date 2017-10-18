4257533d74beddc9ee821986aeb52f2dfba88500
Kemal-Helios <kamalhelios1@gmail.com>
2017-10-18 22:25:28 +0300

    short Link jquery & Bootstrap (CDN)

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -40,8 +40,9 @@
 
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-      <script src="<?php echo $js ?>jquery-3.2.1.min.js"></script>
-      <script src="<?php echo $js ?>bootstrap.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.2.1.min.js"integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="crossorigin="anonymous"></script>
+      <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js  "integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
       <script src="<?php echo $js ?>smooth-scroll.polyfills.min.js"></script>
       <script src="<?php echo $js ?>mixitup.min.js"></script>
       <script src="<?php echo $js ?>plugins.js"></script>
