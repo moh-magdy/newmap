22e3e49d9f1ecbade46719742e194014914781bc
Kemal-Helios <kamalhelios1@gmail.com>
2017-10-28 20:36:11 +0300

    new page

--- layout/js/plugins.js
+++ layout/js/plugins.js
@@ -15,41 +15,9 @@ $(document).ready(function(){
 
 		});
 
-		// init mix-it-up
-		var containerEl = document.querySelector('#container');
-
-		var mixer = mixitup(containerEl, {
-		    controls: {
-		        toggleDefault: 'none'
-		    }
-		});
-
-		mixer.toggleOn('.cat-1');
-
-    // nac active class add !!!!!
-
-    // Get current page URL
-     var url = window.location.href;
-
-     // select file name
-     url = url.substr(url.lastIndexOf("/") + 1);
-
-     // Loop all menu items
-     $('#navto li').each(function(){
-
-      // select href
-      var href = $(this).find('a').attr('href');
-
-      // Check filename
-      if(url == href){
-
-       // Add active class
-       $(this).addClass('active');
-      }
-     });
-
+		
 });
-
+/* Function input is empty  v 1.0 */
 function isNotEmpty(caller) {
     "use strict";
     if (caller.val() === '') {
