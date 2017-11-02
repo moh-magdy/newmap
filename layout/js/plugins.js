5ada3df86c7970a09f111bd9601ec5b834c09b0e
moh-magdy <moh.mgd@gmail.com>
2017-11-02 20:19:07 +0200

    renew loding

--- layout/js/plugins.js
+++ layout/js/plugins.js
@@ -149,7 +149,7 @@ function addMessage(key) {
 // loding screen
 
 $(window).on('load',function() {
-  $('.sk-folding-cube').fadeOut(1000, function () {
+  $('.sk-cube-grid').fadeOut(1000, function () {
     $(this).parent().fadeOut(1000, function () {
       $('body').css("overflow","auto");
       $(this).remove();
